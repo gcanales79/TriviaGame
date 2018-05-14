09a5025bd38e8acdb5abe1880c85796d5908c5e6
Lab Man <gustavo.canales@me.com>
2018-05-13 23:39:34 -0500

    With images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,74 @@
 var disneyQuestion = [{
-    question: "In Aladdin, what is the name of Jasmine's pet tiger?",
-    choices: ["Rajah", "Bo", "Iago", "Jack" ],
-    images:  ["../images/Rajah.gif"],
-    validAnswer: "Rajah"
+    question: "Which country has won the most FIFA World Cups?",
+    choices: ["Brazil", "Italy", "Spain", "Argentina" ],
+    images:  ["assets/images/Brazil.gif"],
+    validAnswer: "Brazil"
     }, {
-    question:"In Peter Pan, Captain Hook had a hook on which part of his     body?",
-    choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
-    validAnswer: "Left Hand"
+    question:"Which is the only country to have reached three finals without winning any?",
+    choices: ["England", "France", "Netherlands", "Czechoslovakia"],
+    images: ["assets/images/Holland.gif"],
+    validAnswer: "Netherlands"
     
     }, {
-    question:"In the Lion King, where does Mufasa and his family live?",
-    choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
-    validAnswer: "Desert"
-    
-    }]
+    question:"Which was the first team to win the World Cup twice?",
+    choices: ["Spain", "Germany", "Brazil", "Italy"],
+    images:["assets/images/Italy.gif"],
+    validAnswer: "Italy"
+    },{
+    question:"Which country holds the record for the worst defense of the World Cup (excluding teams that didn't participate in the subsequent tournament)?",
+    choices: ["Italy in 2006", "Argentina in 1990", "France in 2002", "West Germany in 1994"],
+    images:["assets/images/France.gif"],
+    validAnswer: "France in 2002"
+    },{
+    question:"Which player has scored the most number of goals at the FIFA World Cup?",
+    choices: ["Pele", "Miroslav Klose", "Ronaldo", "Zinedine Zidane"],
+    images:["assets/images/Klose.gif"],
+    validAnswer: "Miroslav Klose"
+    },{ 
+    question:"Who is the youngest player to play in a World Cup final?",
+    choices: ["Norman Whiteside", "Pele", "Diego Maradona", "Zinedine Zidane"],
+    images:["assets/images/Pele.gif"],
+    validAnswer: "Pele"
+    },{
+        question:"Who holds the record of scoring at least one goal in the most number of World Cup matches?",
+        choices: ["Lionel Messi", "Cristiano Ronaldo", "Ronaldo", "Just Fontaine"],
+        images:["assets/images/Ronaldo.gif"],
+        validAnswer: "Ronaldo"
+    },{
+        question:"In which World Cup did players first wear jerseys with their surname on the back?",
+        choices: ["1998", "1990", "1986", "1994"],
+        images:["assets/images/USA1994.gif"],
+        validAnswer: "1994"
+    },{
+        question:"Which country has lost at least three games in three separate World Cup tournaments?",
+        choices: ["Netherlands", "Mexico", "Czechoslovakia", "Trinidad and Tobago"],
+        images:["assets/images/Mexico.gif"],
+        validAnswer: "Mexico" 
+    },{
+        question:"How many countries have won the football World Cup on home soil?",
+        choices: ["None", "5", "6", "8"],
+        images:["assets/images/France2.gif"],
+        validAnswer: "6"   
+    }
+    ]
 
 var questionNumber=0;
 var correctAnswers=0;
 var wrongAnswers=0;
 var unanswered=0;
-var timeToanswer=30;
+var timeToanswer=10;
 var intervalId;
+
+//$("#image").append('<img src="disneyQuestion[0].images[0]">');
+
 
 //Load the question of each section
 function trivia(){
     if(questionNumber<disneyQuestion.length){
         $("#condition").empty();
         $("#startOver").empty();
-        timetoanswer=30;
+        document.getElementById("memory").src = "";
+        timetoanswer=15;
         run();
         $("#question").html(disneyQuestion[questionNumber].question);
         $("#answer1").html(disneyQuestion[questionNumber].choices[0]);
@@ -44,6 +85,7 @@ function trivia(){
 function endOfgame(){
     $("#condition").empty();
     $("#timer").empty();
+    document.getElementById("memory").src = "";
     $("#condition").html("All done here is your final score" + "<br>"
     + "Correct Answers: " + correctAnswers + "<br>"
     + "Incorrect Answers: " + wrongAnswers + "<br>"
@@ -65,19 +107,21 @@ function comparison(answer){
         if(answer==disneyQuestion[questionNumber].validAnswer){
             $(".answer-zone").empty();
             $("#question").empty();
-            $("#condition").text("Correct!!!")
+            $("#condition").html("Correct!!!")
+            document.getElementById("memory").src = disneyQuestion[questionNumber].images[0];
             answer=null;
             questionNumber++;
             correctAnswers++;
-            setTimeout(trivia,3000);
+            setTimeout(trivia,5000);
             }
         else {
             $(".answer-zone").empty();
             $("#question").empty();
             $("#condition").html("Incorrect" + "<br>" + "The correct answer was " +disneyQuestion[questionNumber].validAnswer);
+            document.getElementById("memory").src = disneyQuestion[questionNumber].images[0];
             questionNumber++;
             wrongAnswers++;
-            setTimeout(trivia,3000);
+            setTimeout(trivia,5000);
         }
     }
 
@@ -87,10 +131,11 @@ function timeIsup(){
     $(".answer-zone").empty();
     $("#question").empty();
     $("#condition").html("Your time is up" + "<br>" + "The correct answer was " +disneyQuestion[questionNumber].validAnswer);
+    document.getElementById("memory").src = disneyQuestion[questionNumber].images[0];
     answer=null;
     questionNumber++;
     unanswered++;
-    setTimeout(trivia,3000)
+    setTimeout(trivia,5000)
 }
 
 //Timer for answering the questions
@@ -118,7 +163,7 @@ function run() {
   function stop() {
 
     clearInterval(intervalId);
-    timeToanswer=30;
+    timeToanswer=10;
   }
 
 
